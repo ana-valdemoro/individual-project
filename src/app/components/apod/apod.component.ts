2c9b26905c0be68b1833e24d6ae37f570917a30b
Ana Ramirez <ana_v_r97@hotmail.com>
2020-08-13 20:02:21 +0100

    Doing Apod component, aplying some Css style

--- src/app/components/apod/apod.component.ts
+++ src/app/components/apod/apod.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { APODService } from 'src/app/services/apod.service';
 
+type astronomicData = {
+  date: string;
+  author ?: string;
+  explanation: string;
+  hdurl?: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
 @Component({
   selector: 'app-apod',
   templateUrl: './apod.component.html',
   styleUrls: ['./apod.component.css']
 })
 export class ApodComponent implements OnInit {
-
+  dayPicture: astronomicData = {
+    date: "",
+    explanation : "",
+    hdurl: "",
+    media_type: "",
+    service_version: "",
+    title : "",
+    url:""
+  };
   constructor(private apodService :  APODService) { }
 
   ngOnInit(): void {
+    this.getPicture();
   }
   async getPicture(){
-    let variable = await this.apodService.getAPOD();
-    console.log(variable);
+    this.dayPicture = await this.apodService.getAPOD();
+    console.log(this.dayPicture);
   }
 }
